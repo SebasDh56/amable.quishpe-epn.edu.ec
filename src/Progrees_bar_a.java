a9d620a656ed77a3578edb5b765cbcfe101ab031
Sebatian Quishpe <amable.quishpe@epn.edu.ec>
2022-07-06 16:04:59 -0500

    Examen

---
+++ src/Progrees_bar_a.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.TimeUnit;
+public class Progrees_bar_a {
+    public static void main(String[] args) throws Exception {
+      
+        System.out.print("Examne primer bimestre ");
+		System.out.println("amable.quishpe@epn.edu.ec");
+		int caracter = 26;
+		int porcentaje;
+		int a=1;
+			for (int x = 0; x <= caracter; x++)
+				{
+					for (int i = 1; i <= x; i++)
+					{
+						TimeUnit.SECONDS.sleep((long) 0);
+						System.out.print("#");
+					}
+					porcentaje=(a+x)*4;
+					
+					System.out.println("    "+porcentaje+"%");
+				}
+			}
+		}
+		
+    
+       
+
+
+     
+ 
+    
+
